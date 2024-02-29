64f030d29ed796afb0d62a08917a72fc6cc5adef
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2024-02-29 17:25:53 +0000

    update readme and variable file

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 # Environmental variables
 
-variable "appId" {
-  description = "Azure Kubernetes Service Cluster service principal"
-  type = string
-}
+# variable "appId" {
+#   description = "Azure Kubernetes Service Cluster service principal"
+#   type = string
+# }
 
-variable "password" {
-  description = "Azure Kubernetes Service Cluster password"
-  type = string
-}
+# variable "password" {
+#   description = "Azure Kubernetes Service Cluster password"
+#   type = string
+# }
 
 variable "dbPasswd" {
   description = "Azure Kubernetes Service Cluster password"
