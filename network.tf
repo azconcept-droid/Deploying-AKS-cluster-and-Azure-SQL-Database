9043d7bf04c65945a70c18ae2c782079be88c2cd
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2024-02-29 03:06:30 +0100

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features = {}
+}
+
+resource "azurerm_resource_group" "network" {
+  name     = "sql-network-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "network" {
+  name                = "sql-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
